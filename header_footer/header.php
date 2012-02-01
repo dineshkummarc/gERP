96d8242d3552f32dd40cffca738eae093258b9bd
Harbhag Singh <harbhag.sohal@gmail.com>
2012-02-01 20:57:54 +0530

    some file names changed

---
+++ header_footer/header.php
@@ -0,0 +1,22 @@
+<?php
+$path = $_SERVER['SCRIPT_NAME'];
+$_gERP = explode('gERP',$path);
+$gERP = $_gERP[0]."gERP";
+?>
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<html><head><title>GNDEC ERP SYSTEM</title>
+<link rel="shortcut icon" href="images/favicon.ico" />
+<link rel="stylesheet" type="text/css" href=<?php echo $gERP."/style.css"; ?> />
+<div id="logo" align="center">
+<a href=<?php echo $gERP.'/options.php';?>>
+	<img src=<?php echo $gERP.'/images/logo.png';?> height="100" width="600" alt='logo'></a>
+<table>
+	<tr>
+		<td><a href='options.php'><h2>Home</h2></a></td>
+		<td><a href='index.php?mode=logout'><h2>Logout</h2></a></td>
+	</tr>
+</table>
+</div>
+
+</head>
+<body id='application'>
