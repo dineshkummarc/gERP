beacb0e6b3e808c0fde25917118b17233432ed93
Harbhag Singh <harbhag.sohal@gmail.com>
2012-02-02 15:01:14 +0530

    fixed more broken paths

--- user_options/student_content.php
+++ user_options/student_content.php
@@ -1,28 +1,50 @@
-<p id='introduction'><table align='center'><tr><td><p>Below are The operations you can perform</p></td></tr></table>
+<?php
+$path = $_SERVER['SCRIPT_NAME'];
+$_gERP = explode('gERP',$path);
+$gERP = $_gERP[0]."gERP";
+?>
+<p id='introduction'>
+	<table align='center'>
+		<tr>
+			<td>
+				<p>Below are The operations you can perform</p>
+			</td>
+		</tr>
+	</table>
+</p>
 <div id='student_content'>
 <table class="info" align="center">
-<tr><td>
-<form action="info.php?mode=view_details" method="post">
-<input type='image' src='images/view_details.png'/>
-</form>
-</td><td>
-<form action="info.php?mode=attendence_student" method="post">
-<input type='image' src='images/attendence.png'/>
-</form>
-</td>
-<td>
-<form action="info.php?mode=sessional_marks" method="post">
-<input type='image' src='images/check_marks.png'/>
-</form>
-</td></tr><tr><td>
-<form action="info.php?mode=student_assignment" method="post">
-<input type='image' src='images/assignments.png'/>
-</form>
-</td><td>
-<form action="info.php?mode=user_change_password" method="post">
-<input type='image' src='images/change_password.png'/>
-</form>
-</td><td><form action="student_help.php" method="post">
-<input type='image' src='images/help.png'/>
-</tr></td>
+	<tr>
+		<td>
+			<form action=<?php echo $gERP."/info.php?mode=view_details"; ?> method="post">
+			<input type='image' src=<?php echo $gERP.'/media/images/view_details.png'; ?> />
+			</form>
+		</td>
+		<td>
+			<form action=<?php echo $gERP."/info.php?mode=attendence_student"; ?> method="post">
+			<input type='image' src=<?php echo $gERP.'/media/images/attendence.png'; ?> />
+			</form>
+		</td>
+		<td>
+			<form action=<?php echo $gERP."/info.php?mode=sessional_marks"; ?> method="post">
+			<input type='image' src=<?php echo $gERP.'/media/images/check_marks.png'; ?> />
+			</form>
+		</td>
+	</tr>
+	<tr>
+		<td>
+			<form action=<?php echo $gERP."/info.php?mode=student_assignment"; ?> method="post">
+			<input type='image' src=<?php echo $gERP.'/media/images/assignments.png'; ?> />
+			</form>
+		</td>
+		<td>
+			<form action=<?php echo $gERP."/info.php?mode=user_change_password"; ?> method="post">
+			<input type='image' src=<?php echo $gERP.'/media/images/change_password.png'; ?> />
+			</form>
+		</td>
+		<td>
+			<form action=<?php echo $gERP."/docs/user_docs/student_help.php"; ?> method="post">
+			<input type='image' src=<?php echo $gERP.'/media/images/help.png'; ?> />
+		</td>
+	</tr>
 </table>
--- user_options/tnp_content.php
+++ user_options/tnp_content.php
@@ -1,33 +1,60 @@
-<p id='introduction'><table align='center'><tr><td><p>Below are The operations you can perform</p></td></tr></table>
+<?php
+$path = $_SERVER['SCRIPT_NAME'];
+$_gERP = explode('gERP',$path);
+$gERP = $_gERP[0]."gERP";
+?>
+<p id='introduction'>
+	<table align='center'>
+		<tr>
+			<td><p>Below are The operations you can perform</p></td>
+		</tr>
+	</table>
 <div id='tnp_content'>
 <table class="info" align="center">
-<tr><td>
-<form action="info.php?mode=student_details" method="post">
-<input type='image' src='images/student_details.png'/>
-</form>
-</td><td>
-<form action="info.php?mode=get_report" method="post">
-<input type='image' src='images/get_report.png'/></form>
-</td><td>
-<form action="info.php?mode=tnp_upload_record" method="post">
-<input type='image' src='images/upload_placement_record.png'/>
-</form>
-</td></tr><tr><td>
-<form action="info.php?mode=tnp_training_record" method="post">
-<input type='image' src='images/upload_training_record.png'/></form>
-</td><td>
-<form action="info.php?mode=tnp_edit_record" method="post">
-<input type='image' src='images/edit_records.png'/>
-</form></td><td>
-<form action="info.php?mode=tnp_statistics" method="post">
-<input type='image' src='images/statistics.png'/>
-</form>
-</tr></td><tr><td>
-	<form action="info.php?mode=user_change_password" method="post">
-<input type='image' src='images/change_password.png'/>
-</form>
-</td><td>
-<form action="tnp_help.php" method="post">
-<input type='image' src='images/help.png'/>
-</form>
-</tr></td></table>
+<tr>
+	<td>	
+		<form action=<?php echo $gERP."/info.php?mode=student_details"; ?> method="post">
+		<input type='image' src=<?php echo $gERP.'/media/images/student_details.png'; ?> />
+		</form>
+	</td>
+	<td>
+		<form action=<?php echo $gERP."/info.php?mode=get_report"; ?> method="post">
+		<input type='image' src=<?php echo $gERP.'/media/images/get_report.png'; ?> />
+		</form>
+	</td>
+	<td>
+		<form action=<?php echo $gERP."/info.php?mode=tnp_upload_record"; ?> method="post">
+		<input type='image' src=<?php echo $gERP.'/media/images/upload_placement_record.png'; ?> />
+		</form>
+	</td>
+</tr>
+<tr>
+	<td>
+		<form action=<?php echo $gERP."/info.php?mode=tnp_training_record"; ?> method="post">
+		<input type='image' src=<?php echo $gERP.'/media/images/upload_training_record.png'; ?> />
+		</form>
+	</td>
+	<td>
+		<form action=<?php echo $gERP."/info.php?mode=tnp_edit_record"; ?> method="post">
+		<input type='image' src=<?php echo $gERP.'/media/images/edit_records.png'; ?> />
+		</form>
+	</td>
+	<td>
+		<form action=<?php echo $gERP."/info.php?mode=tnp_statistics"; ?> method="post">
+		<input type='image' src=<?php echo $gERP.'/media/images/statistics.png'; ?> />
+		</form>
+	</td>
+</tr>
+<tr>
+	<td>
+		<form action=<?php echo $gERP."/info.php?mode=user_change_password"; ?> method="post">
+		<input type='image' src=<?php echo $gERP.'/media/images/change_password.png'; ?> />
+		</form>
+	</td>
+	<td>
+		<form action=<?php echo $gERP."/docs/user_docs/tnp_help.php"; ?> method="post">
+		<input type='image' src=<?php echo $gERP.'/media/images/help.png'; ?> />
+		</form>
+	</td>
+</tr>
+</table>
